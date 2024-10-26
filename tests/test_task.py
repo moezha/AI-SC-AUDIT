dbaf0ebe47855c3752cb8e3b5bd5c0a97b164b93
moez.hachicha <moez.hachicha@sofrecom.com>
2024-10-26 06:43:57 -0400

    add makefile and update code based on pylint/pytest results

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,27 +1,15 @@
-# tests/test_task.py
+"""
+Unit tests for the TaskManager and Task classes.
+"""
 
-"""Unit tests for the TaskManager and Task classes."""
-
-import pytest
 from task_manager.task_manager import Task, TaskManager
 
-@pytest.fixture
-def create_task_manager():
-    """Fixture to create a TaskManager instance."""
-    return TaskManager()
 
-def test_create_task(task_manager_instance):
+def test_create_task():
+    task_manager_fixture_instance=TaskManager()
     """Test the creation of a task."""
     task_title = "Test Task"
-    task = task_manager_instance.create_task(task_title)
-    assert isinstance(task, Task)  # Check if the created task is an instance of Task
-    assert task.title == task_title  # Check if the title is set correctly
-    assert not task.completed  # Check if the task is initially not completed
-
-def test_create_task2(task_manager_instance):
-    """Test the creation of a task."""
-    task_title = "Test Task"
-    task = task_manager_instance.create_task(task_title)
-    assert isinstance(task, Task)  # Check if the created task is an instance of Task
-    assert task.title == task_title  # Check if the title is set correctly
-    assert not task.completed  # Check if the task is initially not completed
+    task = task_manager_fixture_instance.create_task(task_title)
+    assert isinstance(task, Task)
+    assert task.title == task_title
+    assert not task.completed
